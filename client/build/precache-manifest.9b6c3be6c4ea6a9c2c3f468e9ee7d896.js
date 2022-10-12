8560caf84eb15ad0af27a42a107282adc1635270
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2022-10-12 22:46:42 +0200

    changes

---
+++ client/build/precache-manifest.9b6c3be6c4ea6a9c2c3f468e9ee7d896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92163762658957d02b8e",
+    "url": "/static/css/main.ee9b3341.chunk.css"
+  },
+  {
+    "revision": "92163762658957d02b8e",
+    "url": "/static/js/main.07e171ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1bb67695b4a5b97cc1f7",
+    "url": "/static/js/2.17d12714.chunk.js"
+  },
+  {
+    "revision": "249cb2ee35b5d812be4fefafbe691310",
+    "url": "/static/media/footer.249cb2ee.gif"
+  },
+  {
+    "revision": "0f1360c82510dd3d43261fd87f1c5cee",
+    "url": "/index.html"
+  }
+];
