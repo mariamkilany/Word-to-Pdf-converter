d2a6e94efd8e69e2c647c2403287c6c91ee75098
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2022-10-12 22:28:39 +0200

    commit

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(fileUpload());
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors)
 app.use(express.static(path.join(__dirname,"/client/build")))
 
 app.get('*',(_,res)=>{
