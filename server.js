9c2db7bc92d211977a566a49874c50bf9b0a0cbb
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2022-10-12 22:31:56 +0200

    change

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors)
-app.use(express.static(path.join(__dirname,"/client/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.get('*',(_,res)=>{
-  res.sendFile(path.join(__dirname,"/client/build/index.html"),(err)=>{
+  res.sendFile(path.join(__dirname,"./client/build/index.html"),(err)=>{
     if(err)
     res.status(500).send(err)
   })
